795a014f7c2c36de5b2a2232a7b69e6ded44d57b
Dr. C V Nageswara Rao <cvnr74@gmail.com>
2024-05-31 14:35:55 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,19 @@
+#calculation of simple interest
+# Principal amount - P
+#R is rate of interest
+#T is time period annually
+#SI is simple interest
+
+echo "Enter the Prinicpal Amount:"
+read P
+
+echo "Enter the Rate of Interest:"
+read R
+
+echo "Enter the time Period per year:"
+read T
+
+SI = expr $P \* $T \* $R / 100
+echo "Simple Interest is: "
+read $SI
 
